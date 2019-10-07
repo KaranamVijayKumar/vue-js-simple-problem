361c28a293bace082458e178448de2cf1f865db9
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-10-06 19:49:53 -0500

    added average component

---
+++ components/average/average.js
@@ -0,0 +1,19 @@
+const avg = (x, y, z) => { return (x + y + z)/3 }
+
+const adder = new Vue({
+  el: '#average',
+  data: {
+    guest: 'Vijay',
+    firstNumber: 5,
+    secondNumber: 3,
+    thirdNumber: 2
+  },
+  computed: {
+    result: function () {
+      const i = parseInt(this.firstNumber)
+      const j = parseInt(this.secondNumber)
+      const k = parseInt(this.thirdNumber)
+      return `${this.guest}, your average is ${avg(i, j, k)}.`
+    }
+  }
+})
